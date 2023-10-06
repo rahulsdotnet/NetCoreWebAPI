4c96cdc06824b40a4415738859677498a61b22cc
Rahul Singh <rahulsdotnet@gmail.com>
2023-10-06 07:16:51 +0530

    Example for Use, Map and Mapwhen extension method for configuring the middleware

--- NetCoreWebAPI/Program.cs
+++ NetCoreWebAPI/Program.cs
@@ -1,4 +1,6 @@
 
+using NetCoreWebAPI;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var allowedOrigin = builder.Configuration.GetSection("AllowedOrigins").Get<string[]>();
@@ -18,8 +20,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//builder.Services.AddTransient<CustomMiddleware>();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -33,6 +37,42 @@ app.UseHttpsRedirection();
 app.UseCors("myAppCors");
 app.UseAuthorization();
 
-app.MapControllers();
+#region Use, Map and MapWhen Extension method example for configuring the middleware
+//4. MapWhen (predicate function if that is true then the associated middleware exec)
 
+//app.MapWhen(context => context.Request.Query.ContainsKey("channel"), HandleMapWhen);
+
+//static void HandleMapWhen(IApplicationBuilder app)
+//{
+//    app.Run(async (context) =>
+//    {
+//        var channel = context.Request.Query["channel"];
+//        await context.Response.WriteAsync($"Channel is : {channel}");
+//    });
+//}
+
+////3. Map(Branch the request pipeline based on match of the given request path
+
+//app.Map("/test1", HandleMap);
+
+//static void HandleMap(IApplicationBuilder app)
+//{
+//    app.Run(async (context) => await context.Response.WriteAsync("Test1 from Map Middleware"));
+//}
+
+////2. Use
+//app.Use(async (context, next) =>
+//{
+//    await context.Response.WriteAsync("This is from Middleware1");
+//    await next.Invoke();
+//});
+
+////1. Run (Terminal)
+
+//app.Run(async (context) => await context.Response.WriteAsync("This is a Terminal middleware"));
+
+#endregion
+
+
+app.MapControllers();
 app.Run();
